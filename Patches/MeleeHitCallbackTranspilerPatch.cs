a19c3311301ed6d60bf70a18e8aca9760791267f
Mike <killeymic@gmail.com>
2024-04-03 15:43:23 -0700

    Refactor (Moved patch to it's own file)

---
+++ Patches/MeleeHitCallbackTranspilerPatch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+
+using TaleWorlds.MountAndBlade;
+
+using HarmonyLib;
+
+namespace PartialParry.Patches
+{
+    [HarmonyPatch]
+    public static class MeleeHitCallbackTranspilerPatch
+    {
+        [HarmonyTranspiler]
+        [HarmonyPatch(typeof(Mission), "MeleeHitCallback")]
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var code = new List<CodeInstruction>(instructions);
+
+            for (int i = 1; i < code.Count - 1; i++)
+            {
+                // we search for the first local set local variable (which is a "flag" who he used to define if the attack as been blocked)
+                if (code[i].opcode == OpCodes.Stloc_0)
+                {
+                    // we make sure that it will be set at false by changing the instruction where the code jump when true
+                    // so that the registerBlow will always be called since the blow is not parried
+                    if (code[i - 1].opcode == OpCodes.Ldc_I4_1)
+                    {
+                        code[i - 1].opcode = OpCodes.Ldc_I4_0;
+                        return code;
+                    }
+                    break;
+                }
+            }
+            return code;
+        }
+    }
+}
