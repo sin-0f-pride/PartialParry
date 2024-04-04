4f91d5aca7a7e616f1e6e1067495e643edfc0200
Mike <killeymic@gmail.com>
2024-04-04 01:31:05 -0700

    Added logging functionality for reporting & testing

--- SubModule.cs
+++ SubModule.cs
@@ -1,6 +1,8 @@
 ﻿using TaleWorlds.MountAndBlade;
 
 using HarmonyLib;
+using System.IO;
+using System;
 
 namespace PartialParry
 {
@@ -16,6 +18,19 @@ namespace PartialParry
         protected override void OnSubModuleUnloaded()
         {
             base.OnSubModuleUnloaded();
+        }
+        public static void Log(string message)
+        {
+            string text = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "Mount and Blade II Bannerlord", "Logs");
+            if (!Directory.Exists(text))
+            {
+                Directory.CreateDirectory(text);
+            }
+            string path = Path.Combine(text, "PartialParry.txt");
+            using (StreamWriter streamWriter = new StreamWriter(path, true))
+            {
+                streamWriter.WriteLine("[" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + "] " + message);
+            }
         }
     }
 }
